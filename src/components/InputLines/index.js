ca5315e8841e751e9dba640a794cf27bec59c7b6
Lucas Rosario <lucas.rosario25@gmail.com>
2020-10-21 16:41:00 -0300

    small fix inputlines

--- src/components/InputLines/index.js
+++ src/components/InputLines/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import * as S from './styles';
 
+function handleFocus(event) {
+  event.target.select();
+}
+
 export default React.forwardRef(
   ({ disabled, onChange, success, value }, ref) => (
     <S.InputGame success={success}>
       <input
+        onFocus={handleFocus}
         ref={ref}
         type="text"
         disabled={disabled}
